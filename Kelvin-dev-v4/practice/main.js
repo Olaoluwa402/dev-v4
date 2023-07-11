25001d73b8f16772093563f311a691071554b3ef
unknown <okparakelvin65@gmail.com>
2023-07-11 12:31:56 +0100

    today

--- Kelvin-dev-v4/practice/main.js
+++ Kelvin-dev-v4/practice/main.js
@@ -1,49 +1,67 @@
-// let whileLoopData_2 = [
-//     {product_title: "title 1", product_price: "$200", product_qty: 1},
-//     {product_title: "title 2", product_price: "$100", product_qty: 2},
-//     {product_title: "title 3", product_price: "$300", product_qty: 3},
-//     {product_title: "title 4", product_price: "$400", product_qty: 4},
-//     {product_title: "title 5", product_price: "$500", product_qty: 5}
-// ]
+// // const listOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15];
 
-// function whileLoopOp(data){
-//     let productPriceData = 0
-//     for(let i = 0; i < data.length; i++){
-//       whileLoopData_2[i].product_price
-//        productrep1 = data[i].product_price.replace("$" , "")
+// // const evenArr = (mainArr) => {
+// //   const evenArr = [];
+// //   for (let i = 0; i < mainArr.length; i++) {
+// //     if (mainArr[i] % 2 == 0) {
+// //       evenArr.push(mainArr[i]);
+// //     }
+// //   }
+// //   return evenArr;
+// // };
 
-//        productrep2 = parseInt(productrep1)
+// // console.log(evenArr(listOfNum));
+// // console.log(listOfNum);
 
-//        productPriceData += productrep2
-      
-       
-//     }
-//     return productPriceData
-// }
+// const person = {
+//   firstName: "Ben",
+//   lastName: "White",
+//   age: 112,
+//   country: "Finland",
+//   city: "Helsinki",
+//   skills: [
+//     "HTML",
+//     "CSS",
+//     "Javascript",
+//     "React",
+//     "Node",
+//     "MongoDB",
+//     "Python",
+//     "D3.js",
+//   ],
+//   isMarried: true,
+// };
 
-// console.log(whileLoopOp(whileLoopData_2))
+// let personString = `${person.firstName}
+// ${person.lastName}
+// ${person.age}
+// ${person.country}
+// ${person.city}
+// ${person["skills"][2]}
+// ${person["isMarried"]}
+// `;
 
-// let num = 1;
-// do{
-//     console.log(num);
-//     num++
-// } while(num <= 5)
+// console.log(personString);
 
-// const digits = [1,2,3,4,5]
+// console.log(
+//   person.firstName,
+//   "\n",
+//   person.lastName,
+//   "\n",
+//   person.age,
+//   "\n",
+//   person.country,
+//   "\n",
+//   person.city,
+//   "\n",
+//   person["skills"][3],
+//   "\n",
+//   person["isMarried"],
+//   "\n"
+// );
 
-// for (const num of digits){
-//     console.log(num * num)
-// }
+let a = { age: 77 };
+let b = a;
+let c = { age: 77 };
 
-const ints = [1, 2, 3, "John", 5, 6, 7, 8, 10]
-
-const newArr = []
-
-for(let i = 0; i < ints.length; i++){
-    if (typeof(ints[i]) === "string" ) {
-        continue
-    }
-    newArr.push(ints[i])
-} 
-
-console.log(newArr)
+console.log(c != a);
