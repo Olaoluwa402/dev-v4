9561c026d26961c73c5d1cd6f00c0fc715919956
user <user@users-MacBook-Pro.local>
2023-07-14 13:15:59 +0100

    deleted content

--- JS-basics-advance/js/testimonial.js
+++ JS-basics-advance/js/testimonial.js
@@ -1,32 +1,3 @@
-// if (document.readyState === "loading") {
-//   document.addEventListener("click", ready);
-// } else {
-//   ready();
-// }
-
-// let index = 0;
-// const slides = document.querySelectorAll(".slide-container"); //returns Nodelist []
-// function ready() {
-//   const nextBtn = document.getElementById("next");
-//   nextBtn.addEventListener("click", next);
-//   const prevBtn = document.getElementById("prev");
-//   prevBtn.addEventListener("click", prev);
-// }
-
-// function next() {
-//   slides[index].classList.remove("active");
-//   index = (index + 1) % slides.length;
-//   console.log('index is ', index)
-//   slides[index].classList.add("active");
-// }
-
-// function prev() {
-//   slides[index].classList.remove("active");
-//   index = (index - 1 + slides.length) % slides.length;
-//   slides[index].classList.add("active");
-// }
-
-
 
 if (document.readyState === "loading") {
   document.addEventListener("click", ready);
