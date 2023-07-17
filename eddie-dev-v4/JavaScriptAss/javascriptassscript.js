bb384090b1cdada9c7cded356ee084a82052dd1e
Eddie Oaikhena <eddieoaikhena@yahoo.com>
2023-07-17 14:28:02 +0100

    assignment

---
+++ eddie-dev-v4/JavaScriptAss/javascriptassscript.js
@@ -0,0 +1,27 @@
+if (document.readyState === "loading") {
+    document.addEventListener("click", ready);
+  } else {
+    ready();
+  }
+  
+  let index = 0;
+  const slides = document.querySelectorAll(".innercontainer"); //returns Nodelist []
+  function ready() {
+    const nextBtn = document.getElementById("next");
+    nextBtn.addEventListener("click", next);
+    const prevBtn = document.getElementById("prev");
+    prevBtn.addEventListener("click", prev);
+  }
+  
+  function next() {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    console.log(index, "index");
+    slides[index].classList.add("active");
+  }
+  
+  function prev() {
+    slides[index].classList.remove("active");
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add("active");
+  }
