985641786c05ffc47fb78dcaf9f5ccc6868a7d18
Eddie Oaikhena <eddieoaikhena@yahoo.com>
2023-07-17 21:30:01 +0100

    assignment

---
+++ eddie-dev-v4/Algorithms/Algorithms.js
@@ -0,0 +1,23 @@
+// write a function to convert celcius to fareheit scale.
+function celciusToFarenheit(param){
+    let result = (9/5)* param + 32;
+    return Math.ceil(result);
+
+}
+console.log(celciusToFarenheit(0))
+
+// write a function to reverse a string.
+function reverseString(str){
+    str=str.split("").reverse().join(""); // using split, join and rever
+    console.log(`The reversed String is: ${str}`);
+}
+reverseString("olleh")
+
+
+// write a function to truncate a string.
+function truncatingString(string) {
+    if (typeof string!== "string" ||!Number.isInteger(+truncateLength)){
+        
+        
+}
+}
