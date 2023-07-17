4be3511e9507bc4b80e92fa5ea5793284de8270a
Eddie Oaikhena <eddieoaikhena@yahoo.com>
2023-07-17 16:29:50 +0100

    assignment

--- eddie-dev-v4/JavaScriptAss/javascriptassscript.js
+++ eddie-dev-v4/JavaScriptAss/javascriptassscript.js
@@ -1,27 +1,30 @@
 if (document.readyState === "loading") {
     document.addEventListener("click", ready);
-  } else {
+} else {
     ready();
   }
   
+  
+  const slides = document.getElementsByClassName("innercontainer"); //returns Nodelist []
   let index = 0;
-  const slides = document.querySelectorAll(".innercontainer"); //returns Nodelist []
   function ready() {
     const nextBtn = document.getElementById("next");
-    nextBtn.addEventListener("click", next);
+    nextBtn.addEventListener("click" , next)
     const prevBtn = document.getElementById("prev");
-    prevBtn.addEventListener("click", prev);
+    prevBtn.addEventListener("click" , prev);
   }
   
   function next() {
-    slides[index].classList.remove("active");
+    console.log(slides)
+    slides[index].classList.remove("Active");
     index = (index + 1) % slides.length;
     console.log(index, "index");
-    slides[index].classList.add("active");
+    slides[index].classList.add("Active");
   }
   
   function prev() {
-    slides[index].classList.remove("active");
+    console.log(slides)
+    slides[index].classList.remove("Active");
     index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add("active");
+    slides[index].classList.add("Active");
   }
