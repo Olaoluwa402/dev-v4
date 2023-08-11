a8e5f68666dc86417efb56895e07a87a11a38748
Eddie Oaikhena <eddieoaikhena@yahoo.com>
2023-08-11 12:42:46 +0100

    project-update

--- eddie-dev-v4/ecommerce-project/wishlistmodal.js
+++ eddie-dev-v4/ecommerce-project/wishlistmodal.js
@@ -1,12 +1,12 @@
-const openbtn = document.getElementById("modalopen")
+const openbtn = document.getElementsByClassName("modalopen")[0]
 const closebtn = document.getElementById("modalclose")
 const modalcontainer = document.getElementById("modalcontainer")
 
 openbtn.addEventListener("click" , function(){
-    console.log(openbtn,modalcontainer)
+    // console.log(openbtn,modalcontainer)
     modalcontainer.style.display = "block"
 })
 closebtn.addEventListener("click" , function(){
-    console.log(closebtn,modalcontainer)
+    // console.log(closebtn,modalcontainer)
     modalcontainer.style.display = "none"
 })
