91bcf4c546c3e6125890b0affe839eca7207b2d0
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-08-15 12:25:42 +0100

    react components

--- learn-react/learn-basics/src/screens/Home.js
+++ learn-react/learn-basics/src/screens/Home.js
@@ -3,13 +3,17 @@ import Navigation from "../components/Navigation/Navigation";
 import Hero from "../components/Hero/Hero";
 import Blog from "../components/Blog/Blog";
 import Footer from "../components/Footer/Footer";
+
+import styles from "./Home.module.css";
 const HomeScreen = () => {
   return (
     <div>
-      {/* navigation */}
-      <Navigation />
-      {/* hero */}
-      <Hero />
+      <section className={styles.header}>
+        {/* navigation */}
+        <Navigation />
+        {/* hero */}
+        <Hero />
+      </section>
       {/* blog */}
       <Blog />
       {/* footer */}
