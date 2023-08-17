c8b1f24320c3e9fb397dcdaf160d75f71a26e724
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-08-17 12:15:44 +0100

    styled component

--- learn-react/learn-basics/src/components/Footer/Footer.jsx
+++ learn-react/learn-basics/src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import styles from "./Footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  const getDate = () => {
+    const date = new Date();
+    const final = `${date.getFullYear()}`;
+    return final;
+  };
+  return (
+    <div className={styles.container}>
+      <p>&copy; {getDate()}, MYCOMOPANY</p>
+    </div>
+  );
 };
 
 export default Footer;
