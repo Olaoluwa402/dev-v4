12358cf5bfb71581a53ff9e8cb2d648d157e3d6f
Joseph <ikeanyi_joseph@yahoo.com>
2023-08-21 12:44:20 +0100

    Updated

--- learn-react/learn-basics/src/components/Hero/Hero.jsx
+++ learn-react/learn-basics/src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import styles from "./Hero.module.css";
 import Button from "../Button/Button";
-import { ButtonStyled } from "../ButtonStyled";
+// import { ButtonStyled } from "../ButtonStyled";
 
 const Hero = () => {
   return (
@@ -19,13 +19,13 @@ const Hero = () => {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
           repellat ratione quas, laboriosam architecto tempora.
         </p>
-        <ButtonStyled color="red" bgColor="green">
+        {/* <ButtonStyled color="red" bgColor="green">
           our list
         </ButtonStyled>
 
         <ButtonStyled color="white" bgColor="purple">
           our list
-        </ButtonStyled>
+        </ButtonStyled> */}
 
         {/* button component */}
         <Button
