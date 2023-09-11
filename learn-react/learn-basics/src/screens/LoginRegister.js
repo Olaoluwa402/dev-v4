e071b5a9fc6b79d69e98751c69dbfa5578249698
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-09-11 13:04:07 +0100

    authentication in react login, register

---
+++ learn-react/learn-basics/src/screens/LoginRegister.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginRegister from "../components/Auth/LoginRegister/LoginRegister";
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <LoginRegister login="login" />
+    </div>
+  );
+};
+
+export default LoginScreen;
