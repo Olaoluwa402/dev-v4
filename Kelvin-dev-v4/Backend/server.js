a7c574807657c922974e4a4b6e513ebe43e519c7
unknown <okparakelvin65@gmail.com>
2023-10-13 06:51:37 +0100

    node commit

--- Kelvin-dev-v4/Backend/server.js
+++ Kelvin-dev-v4/Backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import colors from "colors";
 import httpStatus from "http-status";
+import { dbConnect } from "./config/db.js";
 
 dotenv.config();
 const app = express();
@@ -20,12 +21,21 @@ app.get("/", (req, res) => {
 const port =
   process.env.NODE_ENV === "production" ? process.env.NODE_ENV : 9000;
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(`err: ${err}`.bgRed);
-  }
-  console.log(
-    `Server listening on port: ${port} in ${process.env.NODE_ENV} mode...`
-      .bgGreen
-  );
-});
+dbConnect()
+  .then((res) => {
+    console.log(`db connection sccessful`.bgCyan);
+
+    // listen on port
+    app.listen(port, (err) => {
+      if (err) {
+        console.log(`err: ${err}`.bgRed);
+      }
+      console.log(
+        `Server listening on port: ${port} in ${process.env.NODE_ENV} mode...`
+          .bgGreen
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(`db connection error: ${err.message}`);
+  });
